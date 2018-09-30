ab69e26e2f2ffe8597669eb2b38dfb85abd6cb0c
Ivy Olamit <ivy.olamit@clio.com>
2018-09-29 21:08:26 -0700

    Integrate chat, menu and home content component in the app.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import ChatBot from 'react-simple-chatbot';
+import Chat from "./components/chatbot";
+import Menu from "./components/menu";
+import HomeContent from "./components/homeContent";
 import React, { Component } from 'react';
-import { Steps } from "./DataSet";
 import './App.css';
-
-
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ChatBot steps={Steps} />
+        <Menu/>
+        <HomeContent/>
+        <Chat/>
       </div>
     );
   }
