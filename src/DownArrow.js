18da07141c0f565c5026a335e2987ad3707a0b25
Paul Holden <paul.holden@clio.com>
2018-09-29 20:49:50 -0700

    Reusable Downarrow for different directions

--- src/DownArrow.js
+++ src/DownArrow.js
@@ -1,20 +1,25 @@
 import React from 'react';
 
-const styles = {
-  arrow: {
-    width: 344,
-  },
-  line: {
-    marginLeft: 171,
-    width: 2,
-    background: '#089CFF',
-    height: 148,
-    float: 'center',
-  },
-}
-
 class DownArrow extends React.Component {
   render() {
+    var styles = {
+      arrow: {
+        width: 344,
+      },
+      line: {
+        marginLeft: 171,
+        width: 2,
+        background: '#089CFF',
+        height: 148,
+      },
+    }
+
+    if (this.props.direction == "left") {
+      styles.line.transform = "rotate(220deg)"
+    } else if (this.props.direction == "right") {
+      styles.line.transform = "rotate(160deg)"
+    }
+
     return (
       <div style={styles.arrow}>
         <div style={styles.line}></div>
